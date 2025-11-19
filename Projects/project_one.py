d706bd5f84d223826bc6d93ed8676d17323efc78
Eshaan Mohammed <eshaan.mohammed19@gmail.com>
2025-11-19 09:41:36 +0000

    project_one completed

--- Projects/project_one.py
+++ Projects/project_one.py
@@ -5,7 +5,20 @@ print("You will have to guess the number ")
 
 import random 
 random_integer=random.randint(1, 10)
-
+chances= 3
 
 for i in range(3):
-    user_input=int(input(""))
+    user_input=int(input("what number do you guess"))
+    if random_integer > user_input:
+        print("too big ")
+        chances=chances-1
+        print(chances ,"chances left")
+    elif random_integer < user_input:
+        print("Too small")
+        chances=chances -1 
+        print(chances ,"chances left")
+    elif random_integer == user_input:
+        print("Correct")
+        print("you have " , chances ,  "chances left")
+    else:
+        print("Invalid input")
